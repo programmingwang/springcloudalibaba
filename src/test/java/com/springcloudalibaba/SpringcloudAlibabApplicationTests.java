551c0c8e1817f2fd426796dd4c3847c6681aac2f
zuohong <823178351@qq.com>
2021-02-11 15:17:34 +0800

    first commit

---
+++ src/test/java/com/springcloudalibaba/SpringcloudAlibabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloudalibaba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudAlibabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
